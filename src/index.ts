951d24f0b594e835344b63083733d01a2a39ff14
Roberto <roberdm49@gmail.com>
2023-11-25 14:48:58 -0300

    express and morgan added

--- src/index.ts
+++ src/index.ts
@@ -1,124 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
 
-const prisma = new PrismaClient()
+dotenv.config()
 
-async function populateDB () {
-  console.log("Initialized!")
-  const tenant1 = await prisma.tenant.create({
-    data: {
-      name: 'Dani\'s'
-    }
-  })
+const app = express()
+const port = process.env.APP_PORT
 
-  const tenant2 = await prisma.tenant.create({
-    data: {
-      name: 'Gonza LLC'
-    }
-  })
+app.use(morgan('dev'))
 
-  await prisma.user.create({
-    data: {
-      firstname: 'Roberto',
-      lastname: 'Marcos',
-      username: 'roberdmarcos',
-      password: 'superhashedpassword',
-      tenant: {
-        connect: {
-          id: tenant1.id
-        }
-      }
-    }
-  })
+app.get('/', (request, response) => {
+  response.send('Hello world')
+})
 
-  await prisma.user.create({
-    data: {
-      firstname: 'Micaela',
-      lastname: 'Vanzato',
-      username: 'micavanzato',
-      password: 'superhashedandsecretpassword',
-      tenant: {
-        connect: {
-          id: tenant1.id
-        }
-      }
-    }
-  })
-
-  await prisma.user.create({
-    data: {
-      firstname: 'Gonzalo',
-      lastname: 'Vanzato',
-      username: 'gvanzato123',
-      password: 'superhashedpasswordthethird',
-      tenant: {
-        connect: {
-          id: tenant2.id
-        }
-      }
-    }
-  })
-
-  const currency = await prisma.currency.create({
-    data: {
-      name: 'Pesos Argentinos',
-      isoCode: 'ARS',
-      isoNum: '032'
-    }
-  })
-
-  const company = await prisma.productCompany.create({
-    data: {
-      name: 'Arcor',
-      tenant: {
-        connect: {
-          id: tenant2.id
-        }
-      }
-    }
-  })
-
-  await prisma.product.create({
-    data: {
-      name: 'Block 38g',
-      price: 320,
-      tenant: {
-        connect: {
-          id: tenant2.id
-        }
-      },
-      company: {
-        connect: {
-          id: company.id
-        }
-      },
-      currency: {
-        connect: {
-          id: currency.id
-        }
-      }
-    }
-  })
-
-  await prisma.product.create({
-    data: {
-      name: 'Kinder Maxi',
-      price: 250,
-      tenant: {
-        connect: {
-          id: tenant1.id
-        }
-      },
-      currency: {
-        connect: {
-          id: currency.id
-        }
-      }
-    }
-  })
-}
-
-function main () {
-  // populateDB()
-}
-
-main()
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
---
+++ src/init.ts
@@ -0,0 +1,118 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const populateDB = async () => {
+  console.log("Initialized!")
+  const tenant1 = await prisma.tenant.create({
+    data: {
+      name: 'Dani\'s'
+    }
+  })
+
+  const tenant2 = await prisma.tenant.create({
+    data: {
+      name: 'Gonza LLC'
+    }
+  })
+
+  await prisma.user.create({
+    data: {
+      firstname: 'Roberto',
+      lastname: 'Marcos',
+      username: 'roberdmarcos',
+      password: 'superhashedpassword',
+      tenant: {
+        connect: {
+          id: tenant1.id
+        }
+      }
+    }
+  })
+
+  await prisma.user.create({
+    data: {
+      firstname: 'Micaela',
+      lastname: 'Vanzato',
+      username: 'micavanzato',
+      password: 'superhashedandsecretpassword',
+      tenant: {
+        connect: {
+          id: tenant1.id
+        }
+      }
+    }
+  })
+
+  await prisma.user.create({
+    data: {
+      firstname: 'Gonzalo',
+      lastname: 'Vanzato',
+      username: 'gvanzato123',
+      password: 'superhashedpasswordthethird',
+      tenant: {
+        connect: {
+          id: tenant2.id
+        }
+      }
+    }
+  })
+
+  const currency = await prisma.currency.create({
+    data: {
+      name: 'Pesos Argentinos',
+      isoCode: 'ARS',
+      isoNum: '032'
+    }
+  })
+
+  const company = await prisma.productCompany.create({
+    data: {
+      name: 'Arcor',
+      tenant: {
+        connect: {
+          id: tenant2.id
+        }
+      }
+    }
+  })
+
+  await prisma.product.create({
+    data: {
+      name: 'Block 38g',
+      price: 320,
+      tenant: {
+        connect: {
+          id: tenant2.id
+        }
+      },
+      company: {
+        connect: {
+          id: company.id
+        }
+      },
+      currency: {
+        connect: {
+          id: currency.id
+        }
+      }
+    }
+  })
+
+  await prisma.product.create({
+    data: {
+      name: 'Kinder Maxi',
+      price: 250,
+      tenant: {
+        connect: {
+          id: tenant1.id
+        }
+      },
+      currency: {
+        connect: {
+          id: currency.id
+        }
+      }
+    }
+  })
+}
