06d8e86c2a4f18858704a9cacfaf3d4a487515f6
Roberto Marcos <roberdm49@gmail.com>
2023-11-29 14:24:47 -0300

    try catch user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,22 +5,26 @@ import { UserInterface } from '@/interfaces'
 const prisma = new PrismaClient()
 
 const createUser: RequestHandler = async (request, response) => {
-  const { firstname, lastname, username, password, tenantId }: UserInterface = request.body
-  const user = await prisma.user.create({
-    data: {
-      firstname,
-      lastname,
-      username,
-      password,
-      tenant: {
-        connect: {
-          id: tenantId
+  try {
+    const { firstname, lastname, username, password, tenantId }: UserInterface = request.body
+    const user = await prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        username,
+        password,
+        tenant: {
+          connect: {
+            id: tenantId
+          }
         }
       }
-    }
-  })
+    })
 
-  return user
+    return user
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default { createUser }
