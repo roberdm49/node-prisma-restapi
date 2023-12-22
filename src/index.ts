c4481819648749bf306a0176e765ec47514e8a46
Roberto Marcos <roberdm49@gmail.com>
2023-12-22 13:15:48 -0300

    routes setup modified

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
-import { createAuthRoutes } from '@/core/auth/auth.routes'
-import { createProductsRoutes } from '@/core/products/products.routes'
-import { createUsersRoutes } from '@/core/users/users.routes'
-import { createCompanyRoutes } from './core/company/company.routes'
-import { createDailySaleRoutes } from './core/daily-sale/daily-sale.routes'
-import { createPurchaseRoutes } from './core/purchase/purchase.routes'
-import { createCurrencyRoutes } from './core/currency/currency.routes'
-import AuthModel from './core/auth/auth.model'
-import ProductsModel from './core/products/products.model'
-import UsersModel from './core/users/users.model'
-import CompanyModel from './core/company/company.model'
-import DailySaleModel from './core/daily-sale/daily-sale.model'
-import PurchaseModel from './core/purchase/purchase.model'
-import CurrencyModel from './core/currency/currency.model'
+import { configureRoutes } from './routes'
 
 dotenv.config()
 
@@ -25,21 +12,7 @@ app.use(morgan('dev'))
 app.use(express.json()) // http://expressjs.com/en/api.html#express.json
 app.use(express.urlencoded({ extended: false })) // http://expressjs.com/en/5x/api.html#express.urlencoded
 
-const authModel = new AuthModel()
-const productsModel = new ProductsModel()
-const usersModel = new UsersModel()
-const companyModel = new CompanyModel()
-const dailySaleModel = new DailySaleModel()
-const purchaseModel = new PurchaseModel()
-const currencyModel = new CurrencyModel()
-
-app.use('/auth', createAuthRoutes({ authModel }))
-app.use('/products', createProductsRoutes({ productsModel }))
-app.use('/users', createUsersRoutes({ usersModel }))
-app.use('/company', createCompanyRoutes({ companyModel }))
-app.use('/daily-sale', createDailySaleRoutes({ dailySaleModel }))
-app.use('/purchase', createPurchaseRoutes({ purchaseModel }))
-app.use('/currency', createCurrencyRoutes({ currencyModel }))
+app.use(configureRoutes())
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
---
+++ src/routes.ts
@@ -0,0 +1,39 @@
+import express, { Router } from 'express'
+// Routes creators
+import { createAuthRoutes } from '@/core/auth/auth.routes'
+import { createProductsRoutes } from '@/core/products/products.routes'
+import { createUsersRoutes } from '@/core/users/users.routes'
+import { createCompanyRoutes } from './core/company/company.routes'
+import { createDailySaleRoutes } from './core/daily-sale/daily-sale.routes'
+import { createPurchaseRoutes } from './core/purchase/purchase.routes'
+import { createCurrencyRoutes } from './core/currency/currency.routes'
+// Models
+import AuthModel from './core/auth/auth.model'
+import ProductsModel from './core/products/products.model'
+import UsersModel from './core/users/users.model'
+import CompanyModel from './core/company/company.model'
+import DailySaleModel from './core/daily-sale/daily-sale.model'
+import PurchaseModel from './core/purchase/purchase.model'
+import CurrencyModel from './core/currency/currency.model'
+
+export const configureRoutes = (): Router => {
+  const router = express.Router()
+
+  const authModel = new AuthModel()
+  const productsModel = new ProductsModel()
+  const usersModel = new UsersModel()
+  const companyModel = new CompanyModel()
+  const dailySaleModel = new DailySaleModel()
+  const purchaseModel = new PurchaseModel()
+  const currencyModel = new CurrencyModel()
+
+  router.use('/auth', createAuthRoutes({ authModel }))
+  router.use('/products', createProductsRoutes({ productsModel }))
+  router.use('/users', createUsersRoutes({ usersModel }))
+  router.use('/company', createCompanyRoutes({ companyModel }))
+  router.use('/daily-sale', createDailySaleRoutes({ dailySaleModel }))
+  router.use('/purchase', createPurchaseRoutes({ purchaseModel }))
+  router.use('/currency', createCurrencyRoutes({ currencyModel }))
+
+  return router
+}
