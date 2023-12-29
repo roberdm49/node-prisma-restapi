0cb1120db08f5cfb17aa97674cffeda6f45c89cd
Roberto Marcos <roberdm49@gmail.com>
2023-12-28 21:50:33 -0300

    removed default value for create cookie

--- src/utils/createSecureCookie.ts
+++ src/utils/createSecureCookie.ts
@@ -1,13 +1,11 @@
 import cookie from 'cookie'
 import { GlobalEnv } from '@/constants'
 
-export const createSecureCookie = (cookieName: string, cookieValue: string, maxAgeInSeconds?: number): string => {
-  const DEFAULT_MAX_AGE = 60 * 60 * 24 // A day (the value is in seconds)
-
+export const createSecureCookie = (cookieName: string, cookieValue: string, maxAgeInSeconds: number): string => {
   const cookieOptions = {
     httpOnly: true,
     secure: GlobalEnv.NODE_ENV === 'production',
-    maxAge: maxAgeInSeconds ?? DEFAULT_MAX_AGE
+    maxAge: maxAgeInSeconds
   }
 
   const setCookie = cookie.serialize(cookieName, cookieValue, cookieOptions)
