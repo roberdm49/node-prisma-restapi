2a76692361c4d595b984735c373d471031e69d68
Roberto <roberdm49@gmail.com>
2024-03-02 14:59:37 -0300

    path resolved jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./src/jest/singleton.ts']
+  setupFilesAfterEnv: ['./src/jest/singleton.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
