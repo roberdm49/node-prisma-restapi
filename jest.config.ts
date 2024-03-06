9b7e40eaa0327fcccde544d1d6a1f5935f320d28
Roberto Marcos <roberdm49@gmail.com>
2024-03-06 13:57:44 -0300

    config jest

--- jest.config.ts
+++ jest.config.ts
@@ -8,11 +8,5 @@ export default {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts', // Archivos que quieres incluir en el reporte de cobertura
-    '!src/**/*.spec.ts', // Archivos de pruebas que no quieres incluir en el reporte de cobertura
-    '!src/**/*.test.ts' // Archivos de pruebas que no quieres incluir en el reporte de cobertura
-  ]
+  }
 }
