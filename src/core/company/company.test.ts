661b74ef65ed2d50a85f04cb140d7762bee980a6
Roberto Marcos <roberdm49@gmail.com>
2024-03-09 14:34:12 -0300

    last tests for companies

--- src/core/company/company.test.ts
+++ src/core/company/company.test.ts
@@ -16,7 +16,16 @@ describe('Company', () => {
     password: 'password1'
   }
 
-  let cookies: string[] = []
+  const tenantInformation2 = {
+    tenantName: 'tenant2',
+    username: 'username2',
+    firstname: 'firstname2',
+    lastname: 'lastname2',
+    password: 'password2'
+  }
+
+  let cookies1: string[] = []
+  let cookies2: string[] = []
 
   beforeEach(async () => {
     await cleanUpAll()
@@ -25,27 +34,57 @@ describe('Company', () => {
       .post('/auth/sign-up')
       .send({ ...tenantInformation1 })
 
-    const loginResponse = await api
+    await api
+      .post('/auth/sign-up')
+      .send({ ...tenantInformation2 })
+
+    const loginResponse1 = await api
       .post('/auth/log-in')
       .send({
         username: tenantInformation1.username,
         password: tenantInformation1.password
       })
 
-    cookies = JSON.parse(JSON.stringify(loginResponse.headers['set-cookie']))
+    const loginResponse2 = await api
+      .post('/auth/log-in')
+      .send({
+        username: tenantInformation2.username,
+        password: tenantInformation2.password
+      })
+
+    cookies1 = JSON.parse(JSON.stringify(loginResponse1.headers['set-cookie']))
+    cookies2 = JSON.parse(JSON.stringify(loginResponse2.headers['set-cookie']))
 
     await api
       .post('/company/create')
       .send(mockCompany)
-      .set('Cookie', cookies)
+      .set('Cookie', cookies1)
+
+    await api
+      .post('/company/create')
+      .send(mockCompany)
+      .set('Cookie', cookies2)
   })
 
-  test('Should retrieve all companies', async () => {
+  test('Should not have access to the endpoints without credentials', async () => {
+    await api.get('/company/get').expect(401)
+    await api.post('/company/create').send(mockCompany).expect(401)
+  })
+
+  test('Should retrieve all corresponding companies', async () => {
     const response = await api
       .get('/company/get')
-      .set('Cookie', cookies)
+      .set('Cookie', cookies1)
       .expect(200)
 
     expect(response.body).toHaveLength(1)
+  })
+
+  test('Should not pass a malformed "create" request', async () => {
+    await api
+      .post('/company/create')
+      .send({ name: 'Mock', script: 'some code' })
+      .set('Cookie', cookies1)
+      .expect(400)
   })
 })
