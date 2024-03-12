22e249eb62083ae48dbeae129f07e776a7365464
Roberto Marcos <roberdm49@gmail.com>
2024-03-11 22:49:03 -0300

    refactor unnused code users

--- src/core/users/users.routes.ts
+++ src/core/users/users.routes.ts
@@ -1,15 +1,8 @@
 import express from 'express'
-import { protectedRouteMiddleware } from '@/middlewares/protectedRoute'
-import UsersService from './users.service'
-import UsersController from './users.controller'
 import { UsersCreateRoutes } from './users.types'
 
 export const createUsersRoutes: UsersCreateRoutes = ({ usersRepository }) => {
   const router = express.Router()
-  const usersService = new UsersService({ usersRepository })
-  const usersController = new UsersController({ usersService })
-
-  router.post('/create', [protectedRouteMiddleware], usersController.create)
 
   return router
 }
