3f5e22d6357bceb20c1ab4a5e266dbbaf8d8441b
Roberto Marcos <roberdm49@gmail.com>
2024-03-11 22:33:37 -0300

    users tests first attempt

---
+++ src/core/users/users.test.ts
@@ -0,0 +1,15 @@
+import { cleanUpAll } from '@/utils/testing'
+
+describe('Users', () => {
+  beforeEach(async () => {
+    await cleanUpAll()
+  })
+
+  describe('Happy paths', () => {
+
+  })
+
+  describe('Exception paths', () => {
+
+  })
+})
